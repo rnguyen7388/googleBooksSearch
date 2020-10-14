0f4f6766b5cc8990a4a684255131936b382c33c3
Ray <rnguyen7388@gmail.com>
2020-10-14 00:29:56 -0500

    added structure

---
+++ googlebooksearch/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/nav";
+import Header from "./components/header";
+import Wrapper from "./components/wrapper";
+import Search from "./pages/search";
+import Saved from "./pages/saved";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+          </Wrapper>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
